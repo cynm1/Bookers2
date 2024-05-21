280362ad1631c57a9579ada95516f7874b12cb0e
cynm1 <163312272+cynm1@users.noreply.github.com>
2024-05-21 15:15:19 +0000

    user/update機能まで作成済み 5/21

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,25 @@ class UsersController < ApplicationController
     @user = current_user
     @users = User.all
   end
-  
+
   def show
     @user = current_user
     @books = Book.all
   end
 
   def edit
+    @user = current_user
   end
   
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
   private
   def user_params
-    params.require(:user).permit(:profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy, :update]
-  resources :users, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit, :update]
 end
